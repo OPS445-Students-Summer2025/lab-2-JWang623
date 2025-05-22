718e997eaf8abd62e7eea431aefbd53357cbffaa
JunLong Wang <jwang557@myseneca.ca>
2025-05-22 16:13:19 -0400

    lab2 complete

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: JunLong Wang
+#Author ID: 176512234
+#Date Created: 2025/05/22
+
+timer = 10
+
+while timer !=0:
+	print(timer)
+	timer = timer - 1
+print("blast off!") 
---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: JunLong Wang
+#Author ID: 176512234
+#Date Created: 2025/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer !=0:
+	print(timer)
+	timer = timer - 1
+print("blast off!") 
